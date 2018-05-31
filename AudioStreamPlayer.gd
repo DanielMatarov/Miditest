361f90ba4eb0141ad7dab7e888c5a5d337b9778d
Daniel Matarov <DMatarov@outlook.com>
2018-05-31 13:05:20 +0100

    Midi Test

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -1,16 +1,10 @@
 extends AudioStreamPlayer
-var midi=MidiStream.new()
-AudioStreamPlayer.set_stream(midi)
-
-
 
 func _ready():
-	
-	AudioStreamPlayer.playing(true)
-	pass
+	 
+	 stream.set_filename("florestan-subset.sf2") 
+	 stream.note_on(50, 0.5)
 
 func _process(delta):
-	midi.note_on(50, 50)
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
+
 	pass
