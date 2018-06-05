cb28a966ea2b43a0c26357472c914a4a2f19684a
Daniel Matarov <DMatarov@outlook.com>
2018-06-05 13:07:13 +0100

    added a set_preset function

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -3,7 +3,8 @@ extends AudioStreamPlayer
 func _ready():
 	 
 	 stream.set_filename("florestan-subset.sf2") 
-	 stream.note_on(50, 0.5)
+	 stream.set_preset(2)
+	 stream.note_on(60, 0.5)
 
 func _process(delta):
 
