1e75e305235d93731246d0d1bf9ff928501ad024
Daniel Matarov <DMatarov@outlook.com>
2018-06-11 11:03:43 +0100

    midi files test

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -3,8 +3,7 @@ extends AudioStreamPlayer
 func _ready():
 	 
 	 stream.set_filename("florestan-subset.sf2") 
-	 stream.set_preset(2)
-	 stream.note_on(60, 0.5)
+	 stream.set_midi_filename("venture.mid")
 
 func _process(delta):
 
