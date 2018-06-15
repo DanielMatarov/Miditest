027a0c2e274bb4b883e00cb48359b2bdab09876c
Daniel Matarov <DMatarov@outlook.com>
2018-06-15 12:50:27 +0100

    Midi File Playback

--- AudioStreamPlayer.gd
+++ AudioStreamPlayer.gd
@@ -4,7 +4,13 @@ func _ready():
 	 
 	 stream.set_filename("florestan-subset.sf2") 
 	 stream.set_midi_filename("venture.mid")
+	
 
 func _process(delta):
 
 	pass
+
+
+func _on_Button_pressed():
+	play(0.0)
+	pass # replace with function body
